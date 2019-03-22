27198010000b50b249e2fcbc3e54c37a7d7182d7
VeraaK <karenveraa@live.be>
2019-03-22 18:06:51 +0100

    caio

--- 6 Hibernate/oefening 1/src/main/java/edu/ap/karenveraa/App.java
+++ 6 Hibernate/oefening 1/src/main/java/edu/ap/karenveraa/App.java
@@ -6,6 +6,8 @@ package edu.ap.karenveraa;
  */
 public class App {
     public static void main(String[] args) {
-        // Battle bat = new Battle();
+        Battle bat = new Battle("Fist of 2019", 2019, "Serieus");
+        Battle battre = new Battle("Beef", 1998, "Discussie");
+        Battle old = new Battle("Ages ago", 1958, "Really nothing");
     }
 }
