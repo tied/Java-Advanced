a9e4ab9d3136ff934814d2c34d670233dc328d1b
VeraaK <karenveraa@live.be>
2019-03-22 17:04:37 +0100

    hibernateconfigfile

---
+++ 6 Hibernate/oefening 1/src/main/java/edu/ap/karenveraa/battle.java
@@ -0,0 +1,12 @@
+package edu.ap.karenveraa;
+
+/**
+ * battle
+ */
+public class battle {
+
+    private String name;
+    private int year;
+    private String type;
+
+}
---
+++ 6 Hibernate/oefening 1/src/main/java/edu/ap/karenveraa/house.java
@@ -0,0 +1,9 @@
+package edu.ap.karenveraa;
+
+/**
+ * house
+ */
+public class house {
+
+    private String name;
+}
